bfc7f0db3a35e90c6d9d4cf8c32f75cf0de6c1da
qingshan <24538474+lniche@users.noreply.github.com>
2024-11-11 23:20:30 +0800

    update

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -7,13 +7,15 @@ pub mod service;
 use crate::pkg::core::config;
 
 pub async fn serve() {
-    let addr = config::global().get_int("app.port").unwrap_or(8000);
+    let port = config::global().get_int("app.port").unwrap_or(8000);
+    let host = config::global()
+        .get_string("app.host")
+        .unwrap_or("0.0.0.0".to_string());
+    let host_port = format!("{}:{}", host, port);
+    let docs_addr = format!("{}/swagger-ui/index.html", host_port);
+    tracing::info!("server start host = http://{}", host_port);
+    tracing::info!("docs addr = http://{}", docs_addr);
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", addr))
-        .await
-        .unwrap();
-
-    tracing::info!("listening on {}", addr);
-
+    let listener = tokio::net::TcpListener::bind(host_port).await.unwrap();
     axum::serve(listener, router::init()).await.unwrap();
 }
